1eade40973df79ee852be036daea69057f927915
roong-ji <mhienegjyuu@naver.com>
2025-11-21 11:03:29 +0900

    refactor: 인터페이스 및 열거형 삭제

--- Assets/02. Scipts/Monster/MonsterData.cs
+++ Assets/02. Scipts/Monster/MonsterData.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewMonsterData", menuName = "Monster System/Monster Data")]
 public class MonsterData : ScriptableObject
 {
-    [Header("몬스터 이름")]
-    [SerializeField] private EMonsterType _monsterType;
-
     [Header("체력")]
     [SerializeField] private float _maxHealth;
 
@@ -15,7 +12,6 @@ public class MonsterData : ScriptableObject
     [Header("이동 속도")]
     [SerializeField] private float _moveSpeed;
 
-    public EMonsterType MonsterType => _monsterType;
     public float MaxHealth => _maxHealth;
     public float AttackPower => _attackPower;
     public float MoveSpeed => _moveSpeed;
