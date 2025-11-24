21d976717ad572163dd259bdfbbb12243a01efd0
OmarChoi <omarchoi80@gmail.com>
2025-11-24 16:20:20 +0900

    feat: 화살이 관통되게 변경

--- Assets/02. Scipts/InGame/Projectile/Arrow.cs
+++ Assets/02. Scipts/InGame/Projectile/Arrow.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    [Header("공격")]
+    private int _attackCount = 0;
     [SerializeField] private float _damage;
+    [SerializeField] private int _maxAttackCount = 4;
+
+    [Header("이동")]
+    private int _direction = -1;
     [SerializeField] private float _arrowSpeed = 10.0f;
     [SerializeField] private float _destroyTime = 5.0f;
-    private int _direction = -1;
+
     private SpriteRenderer _renderer;
 
     private void Awake()
@@ -30,9 +36,14 @@ public class Arrow : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy") == false) return;
-        other.GetComponent<MonsterStats>()?.TakeDamage(_damage);
-
-        // NOTE : 이후 Pooling 방식을 사용해 오브젝트 관리
-        Destroy(this.gameObject);
+        MonsterStats stat = other.GetComponent<MonsterStats>();
+        if (stat == null) return;
+        stat.TakeDamage(_damage);
+        _attackCount++;
+        if (_attackCount >= _maxAttackCount)
+        {
+            // NOTE : 이후 Pooling 방식을 사용해 오브젝트 관리
+            Destroy(this.gameObject);
+        }
     }
 }
