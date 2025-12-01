1d8734875e0befdbbb5aa72b6340717c253d6006
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-12-02 02:08:26 +0900

    feat / 씬 초반 연출

--- Assets/02. Scipts/Lobby/UI/LobbyUIController.cs
+++ Assets/02. Scipts/Lobby/UI/LobbyUIController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,9 +7,61 @@ public class LobbyUIController : MonoBehaviour
 {
     public const string Button = "Button";
 
+    [Header("씬 전환 연출")]
+    [Space]
+    [SerializeField] private float _duration = 3.0f;
+    [Space]
+    [SerializeField] private GameObject _loadingScene;
+    [SerializeField] private TextMeshProUGUI _loadingText;
+
+    private IEnumerator ProcessLoadingEffect()
+    {
+        AsyncOperation loadingOperation = SceneLoader.Instance.LoadSceneAsync(ESceneType.InGame);
+        
+        if (loadingOperation == null)
+        {
+            yield break;
+        }
+
+        loadingOperation.allowSceneActivation = false;
+
+        AudioManager.Instance.Stop(AudioType.BGM);
+        _loadingScene.SetActive(true);
+
+        float timeElapsed = 0;
+        Color startColor = _loadingText.color;
+
+        while(timeElapsed < _duration)
+        {
+            float ratio = timeElapsed / _duration;
+
+            Color newColor = startColor;
+            newColor.a = ratio;
+            _loadingText.color = newColor;
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        while (true)
+        {
+            if (loadingOperation.isDone)
+            {
+                break;
+            }
+
+            if (loadingOperation.progress >= 0.9f)
+            {
+                loadingOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
     public void OnClickPlayButton()
     {
-        SceneLoader.Instance.LoadSceneAsync(ESceneType.InGame);
+        StartCoroutine(ProcessLoadingEffect());
         AudioManager.Instance.Play(AudioType.SFX, Button);
     }
 
