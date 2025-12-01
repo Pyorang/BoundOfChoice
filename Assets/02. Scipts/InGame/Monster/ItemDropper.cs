c9e0ebee461610824ba9e4ce93a3d33f23e1fa43
roong-ji <mhienegjyuu@naver.com>
2025-12-01 22:18:15 +0900

    feat: 아이템 드롭 연출 추가

--- Assets/02. Scipts/InGame/Monster/ItemDropper.cs
+++ Assets/02. Scipts/InGame/Monster/ItemDropper.cs
@@ -5,14 +5,41 @@ public class ItemDropper : MonoBehaviour
     [Header("아이템 드롭 설정")]
     [Space]
     [SerializeField] private EItemType _dropItem;
+    [SerializeField] private int _dropCount;
     [SerializeField] private float _dropChance;
-    
+
+    [Header("드롭 연출 설정")]
+    [Space]
+    [SerializeField] private float _dropForce;
+    [SerializeField] private float _minYForce;
+
     public void TryDropItem()
+    {
+        for (int i = 0; i < _dropCount; ++i)
+        {
+            DropItem();
+        }
+    }
+
+    private void DropItem()
     {
         if (Random.value > _dropChance) return;
 
         Vector2 dropPosition = Vector2.zero;
         dropPosition.x = transform.position.x;
-        ItemSpawner.Instance.CreateItem(_dropItem, dropPosition);
+        GameObject item = ItemSpawner.Instance.CreateItem(_dropItem, dropPosition);
+
+        Rigidbody2D itemRigidbody = item.GetComponent<Rigidbody2D>();
+
+        if (itemRigidbody == null) return;
+
+        Vector2 dropDirection = Random.insideUnitCircle.normalized;
+
+        if (dropDirection.y < _minYForce)
+        {
+            dropDirection.y = _minYForce;
+        }
+
+        itemRigidbody.AddForce(dropDirection * _dropForce, ForceMode2D.Impulse);
     }
 }
