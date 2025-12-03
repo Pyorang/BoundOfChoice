0d95d634d3a821f07247a4cba537b65791fad548
roong-ji <mhienegjyuu@naver.com>
2025-12-03 09:52:38 +0900

    refactor: 아이템 오브젝트 풀링 적용

--- Assets/02. Scipts/InGame/Monster/ItemDropper.cs
+++ Assets/02. Scipts/InGame/Monster/ItemDropper.cs
@@ -4,14 +4,9 @@ public class ItemDropper : MonoBehaviour
 {
     [Header("아이템 드롭 설정")]
     [Space]
-    [SerializeField] private EItemType _dropItem;
+    [SerializeField] private EPoolType _dropItem;
     [SerializeField] private int _dropCount;
     [SerializeField] private float _dropChance;
-
-    [Header("드롭 연출 설정")]
-    [Space]
-    [SerializeField] private float _dropForce;
-    [SerializeField] private float _minYForce;
 
     public void TryDropItem()
     {
@@ -27,19 +22,6 @@ public class ItemDropper : MonoBehaviour
 
         Vector2 dropPosition = Vector2.zero;
         dropPosition.x = transform.position.x;
-        GameObject item = ItemSpawner.Instance.CreateItem(_dropItem, dropPosition);
-
-        Rigidbody2D itemRigidbody = item.GetComponent<Rigidbody2D>();
-
-        if (itemRigidbody == null) return;
-
-        Vector2 dropDirection = Random.insideUnitCircle.normalized;
-
-        if (dropDirection.y < _minYForce)
-        {
-            dropDirection.y = _minYForce;
-        }
-
-        itemRigidbody.AddForce(dropDirection * _dropForce, ForceMode2D.Impulse);
+        GameObject item = ItemSpawner.Instance.SpawnItem(_dropItem, dropPosition);
     }
 }
