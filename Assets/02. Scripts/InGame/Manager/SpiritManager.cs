e07f9af2efa5033b74e23420374d02e290b654a4
Omarchoi <omarchoi80@gmail.com>
2025-12-04 19:10:46 +0900

    feat: 여러 개의 Spirit을 획득할 때 순차적으로 생성하게 변경

--- Assets/02. Scripts/InGame/Manager/SpiritManager.cs
+++ Assets/02. Scripts/InGame/Manager/SpiritManager.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class SpiritManager : SingletonBehaviour<SpiritManager>
 {
     private int _currentSpiritCount;
-    [SerializeField] private int _maxSpiritCount;
     public static event Action<int, int> OnSpiritCountValueChanged;
     public static event Action OnSpiritGained;
     public static event Action OnSpiritCompleted;
+
+    [Header("승리 조건")]
+    [Space]
+    [SerializeField] private int _maxSpiritCount;
+
+    [Header("소환 딜레이")]
+    [Space]
+    [SerializeField] private float _spiritSpawnDelay = 0.25f;
+    private Coroutine _spiritSpawnCoroutine;
 
     protected override void Init()
     {
@@ -20,9 +29,24 @@ public class SpiritManager : SingletonBehaviour<SpiritManager>
     public void GetSpirit(int amount)
     {
         if (amount <= 0) return;
-        AudioManager.Instance.Play(AudioType.SFX, "Spirit");
-        _currentSpiritCount = Mathf.Min(_currentSpiritCount + amount, _maxSpiritCount);
-        OnSpiritGained?.Invoke();
+        if (_spiritSpawnCoroutine != null)
+        {
+            StopCoroutine(_spiritSpawnCoroutine);
+        }
+        _spiritSpawnCoroutine = StartCoroutine(SpawnSpirit(amount));
+    }
+
+    private IEnumerator SpawnSpirit(int amount)
+    {
+        WaitForSeconds delay = new WaitForSeconds(_spiritSpawnDelay);
+        for (int i = 0; i < amount; i++)
+        {
+            AudioManager.Instance.Play(AudioType.SFX, "Spirit");
+            _currentSpiritCount = Mathf.Min(_currentSpiritCount + 1, _maxSpiritCount);
+            OnSpiritGained?.Invoke();
+            yield return delay;
+        }
+        _spiritSpawnCoroutine = null;
     }
 
     public void FillRemainingSpirit()
